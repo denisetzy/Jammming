d6dda7c8d28fed3782111761d46c753df0778da9
denisetzy <denisetzy@gmail.com>
2017-10-08 21:45:20 +0800

    Create Playlist.js

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {TrackList} from '../TrackList/TrackList';
+
+export class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onNameChange = this.onNameChange.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="Playlist">
+        <input onChange={this.handleNameChange} />
+        <TrackList isRemoval={true} onRemove={this.props.onRemove} tracks={this.props.playlistTracks}/>
+        <a className="Playlist-save" onClick={this.props.onSave} >SAVE TO SPOTIFY</a>
+      </div>
+    );
+  }
+
+  handleNameChange(event){
+    this.props.onNameChange(event.target.value);
+  }
+}
