33705feb43ebc716cb1938f491886268975749dc
denisetzy <denisetzy@gmail.com>
2017-10-09 22:09:16 +0800

    Update SearchBar.js

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,16 +4,19 @@ import React from 'react';
 export class SearchBar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchTerm: ''
+    }
     this.search = this.search.bind(this);
     this.this.search = this.this.search.bind(this);
   }
 
   search() {
-    this.props.onSearch(this.state.term)
+    this.props.onSearch(this.state.searchTerm)
   }
 
   handleTermChange(event) {
-    this.setState({this.search.state.term: event.target.value});
+    this.setState({searchTerm: event.target.value});
   }
 
   render() {
